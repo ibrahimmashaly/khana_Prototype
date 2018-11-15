cc83474f55213ee42a0432322eda460d0ae81e2a
David Truong <david@redu.us>
2018-11-15 18:34:08 +0100

    WIP: Audit file iterations and documentation

--- Khana POC/src/Shared/Audit.js
+++ Khana POC/src/Shared/Audit.js
@@ -1,46 +1,51 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import ipfs from '../utils/ipfs';
 
 class Audit extends Component {
 
+    /**
+     * @dev 'createEmptyAuditJson' function is used to create the initial audit
+     * dictionary for a community. The dictionary should ultimately be converted
+     * to a JSON file that is stored on IPFS.
+     * This function also serves as a reference format for all audit files.
+     * When the format is updated here, default values should also be set in
+     * 'DappTokenShared.js' -> defaultState and setupContracts().
+     * 
+     * Explanation of each JSON element:
+     * 
+     * "khanaInfo": Information about the version of Khana they are using and
+     * when/if their contract was upgraded to a major framework change.
+     * @param "version": a version number of the current Khana front end it is using.
+     * @param "lastUpgradeBlock": the blockNumber of when this Khana implementation
+     * was last upgraded.
+     * 
+     * "tokenInfo": Information about the community's contracts at this point in time.
+     * 
+     * "tokenAdmin": Audit logs of the admin actions that have been taken.
+     * @param "addAdmin": an array of the details of each time an admin was added.
+     * @param "removeAdmin": an array of the details of each time an admin was removed.
+     * @param "emergencyStop": an array of the details of each time the emergencyStop
+     * was activated or de-activated.
+     * @param "moveFunds": an array of the details of each time vault funds were moved.
+     * @param "auditChain": an array of the last 5 IPFS hashes of the audit logs that
+     * have been confirmed on the blockchain.
+     * @param "previousImportedAuditHashes": an array of the IPFS hashes of previous
+     * audit files. These are recorded when the system is upgraded and we need to 
+     * import previous balances and records.
+     * 
+     * "tokenActivity": All the transactional records of admins when creating and
+     * destroying tokens.
+     * @param "burns": an array of the details of each admin initiated burn.
+     * e.g. If for some reason an admin needs to penalise a member
+     * and forceably burn a portion of their tokens.
+     * @param "awardsBulk": an array of the details of each bulk award that is given.
+     * e.g. Awarding 100 tokens to everyone who came to an event.
+     * @param "awards": an array of the details of each award that is given.
+     * e.g. Awarding 100 tokens to one person for completing a grant.
+     * 
+     * @return The IPFS hash of the latest audit file.
+    */
     createGenesisAuditJson = async () => {
-
-        // @dev:            'createEmptyAuditJson' function is used to create the initial audit 
-        //                  dictionary for a community. The dictionary should ultimately be converted
-        //                  to a JSON file that is stored on IPFS.
-        //                  This function also serves as a reference format for all audit files.
-        //                  When the format is updated here, default values should also be set in
-        //                  'DappTokenShared.js' -> defaultState and setupContracts()
-        //
-        // khanaInfo:       Information about the version of Khana they are using and 
-        //                  when/if their contract was upgraded to a major framework change.
-        //                  "version": a version number of the current Khana front end it is using.
-        //                  "lastUpgradeBlock": the blockNumber of when this Khana implementation
-        //                          was last upgraded.
-        //
-        // tokenInfo:       Information about the community's contracts at this point in time.
-        //              
-        // tokenAdmin:      Audit logs of the admin actions that have been taken.
-        //                  "addAdmin": an array of the details of each time an admin was added.
-        //                  "removeAdmin": an array of the details of each time an admin was removed.
-        //                  "emergencyStop": an array of the details of each time the emergencyStop 
-        //                          was activated or de-activated.
-        //                  "moveFunds": an array of the details of each time vault funds were moved.
-        //                  "auditChain": an array of the last 5 IPFS hashes of the audit logs that
-        //                          have been confirmed on the blockchain.
-        //                   "previousImportedAuditHashes": an array of the IPFS hashes of previous
-        //                          audit files. These are recorded when the system is upgraded
-        //                          and we need to import previous balances and records.
-        // 
-        // tokenActivity:   "burns": an array of the details of each admin initiated burn.
-        //                          e.g. If for some reason an admin needs to penalise a member
-        //                          and forceably burn a portion of their tokens.
-        //                  "awardsBulk": an array of the details of each bulk award that is given.
-        //                          e.g. Awarding 100 tokens to everyone who came to an event.
-        //                  "awards": an array of the details of each award that is given.
-        //                          e.g. Awarding 100 tokens to one person for completing a grant.
-        //
-
         let defaultJson = {
             "khanaInfo": {
                 "version": this.props.state.app.version,
@@ -72,7 +77,19 @@ class Audit extends Component {
         return defaultJson
     }
 
+    /**
+     * @dev Used for creating and uploading the latest audit file.
+     * @param auditDict A JS dictionary of what should be included in the audit file.
+     * @return The IPFS hash of the latest audit file.
+    */
     createAndUploadNewAuditFile = async (auditDict) => {
+
+        let currentIpfsHash = this.props.state.contract.latestIpfsHash
+        auditDict.tokenAdmin.auditChain.unshift(currentIpfsHash)
+
+        if (auditDict.tokenAdmin.auditChain.length > 5) {
+            auditDict.tokenAdmin.auditChain.pop()
+        }
 
         let ipfsContent = {
             path: '/khana/' + this.props.state.contract.tokenName,
@@ -83,25 +100,13 @@ class Audit extends Component {
 
         // Write description to IPFS, return hash
         const results = await ipfs.add(ipfsContent)
-
         return results[0].hash
     }
 
-    // loadJson = new Promise(function (resolve, reject) {
-    //     var auditFile = '';
-    //     var xmlhttp = new XMLHttpRequest();
-    //     xmlhttp.onreadystatechange = function () {
-    //         if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
-    //             auditFile = xmlhttp.responseText;
-    //             resolve(auditFile)
-    //         }
-    //     };
-    //     // xmlhttp.open("GET", "https://gist.githubusercontent.com/mrdavey/993fc9e52c8a0f57b5e2b5dbaaf49c65/raw/e078ab9280ce487f72b5765230ce6e2689fcd999/audit_mock", true);
-    //     xmlhttp.open("GET", "https://gateway.ipfs.io/ipfs/QmV7FFUNZBqbxV6ba47D3Fj9APKZH1RCJYeRA3mw6XsGVM", true);
-
-    //     xmlhttp.send();
-    // })
-
+    /**
+     * @dev Used for fetching the latest audit file (or a new one if none exists).
+     * @return A JS dictionary of the audit file.
+    */
     getAuditJson = async () => {
         let latestIpfsHash = this.props.state.contract.latestIpfsHash
         let auditJson
@@ -117,8 +122,13 @@ class Audit extends Component {
         return auditJson
     }
 
-    // @dev: returns IPFS hash of new audit file with record of award
-
+    /**
+     * @dev Used for awarding a single person.
+     * @param toAddress Address of the person being awarded.
+     * @param amount The amount to award them.
+     * @param reasons The reason for the award.
+     * @return IPFS hash of new audit file.
+    */
     recordAward = async (toAddress, amount, reason) => {
         let auditHistory = await this.getAuditJson()
 
@@ -131,16 +141,43 @@ class Audit extends Component {
         }
 
         auditHistory.tokenActivity.awards.unshift(newSingleAward)
-
         return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
-    recordBulkAward = async () => {
+    /**
+     * @dev Used for awarding a multiple people for multiple amounts.
+     * @notice `amounts` must be an array of either a single amount or multiple amounts
+     * with the same length as the accounts array.
+     * @param accounts Array of the addresses of the people being awarded.
+     * @param amounts Array of the amounts to award people. See @notice.
+     * @param reasons The reason for the bulk award.
+     * @return IPFS hash of new audit file.
+    */
+    recordBulkAward = async (accounts, amounts, reason) => {
+        let auditHistory = await this.getAuditJson()
 
+        let newBulkAward = {
+            "timeStamp": Date.now(),
+            "toAddress": accounts,
+            "adminAddress": this.props.state.user.currentAddress,
+            "sameAmount": amounts.length === 1,
+            "amountEach": amounts,
+            "reason": reason
+        }
+
+        auditHistory.tokenActivity.awardsBulk.unshift(newBulkAward)
+        return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
-    // @dev: returns IPFS hash of new audit file with record of burn
-
+    /**
+     * @dev Used for burning tokens from an address.
+     * @notice We purposes do not have bulkBurns as this should not really
+     * be used (we support full ownership of assets).
+     * @param toAddress Address of the person being awarded.
+     * @param amount The amount to award them.
+     * @param reasons The reason for the award.
+     * @return IPFS hash of new audit file.
+    */
     recordBurn = async (toAddress, amount, reason) => {
         let auditHistory = await this.getAuditJson()
 
@@ -156,16 +193,64 @@ class Audit extends Component {
         return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
-    recordAddAdmin = async () => {
+    /**
+     * @dev Used for adding a new admin.
+     * @param address Address of the new admin.
+     * @param reasons The reason for the promotion.
+     * @return IPFS hash of new audit file.
+    */
+    recordAddAdmin = async (address, reason) => {
+        let auditHistory = await this.getAuditJson()
 
+        let newAdmin = {
+            "timeStamp": Date.now(),
+            "toAddress": address,
+            "adminAddress": this.props.state.user.currentAddress,
+            "reason": reason
+        }
+
+        auditHistory.tokenAdmin.addAdmin.unshift(newAdmin)
+        return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
-    recordRemoveAdmin = async () => {
+    /**
+     * @dev Used for removing an admin.
+     * @param address Address of the admin to be removed.
+     * @param reasons The reason for the removal.
+     * @return IPFS hash of new audit file.
+    */
+    recordRemoveAdmin = async (address, reason) => {
+        let auditHistory = await this.getAuditJson()
 
+        let removeAdmin = {
+            "timeStamp": Date.now(),
+            "toAddress": address,
+            "adminAddress": this.props.state.user.currentAddress,
+            "reason": reason
+        }
+
+        auditHistory.tokenAdmin.removeAdmin.unshift(removeAdmin)
+        return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
-    recordEmergencyStop = async (isActive) => {
+    /**
+     * @dev Used for activating or de-activating the emergency stop
+     * @param isActiving Bool of what action is being taken when calling.
+     * @param reasons The reason for the removal.
+     * @return IPFS hash of new audit file.
+    */
+    recordEmergencyStop = async (isActiving, reason) => {
+        let auditHistory = await this.getAuditJson()
 
+        let emergencyStop = {
+            "timeStamp": Date.now(),
+            "adminAddress": this.props.state.user.currentAddress,
+            "isActivated": isActiving,
+            "reason": reason
+        }
+
+        auditHistory.tokenAdmin.emergencyStop.unshift(emergencyStop)
+        return await this.createAndUploadNewAuditFile(auditHistory)
     }
 
     recordImportAuditFile = async () => {
@@ -175,6 +260,23 @@ class Audit extends Component {
         // update totalTokenSupply
     }
 
+
+
+
+    // loadJson = new Promise(function (resolve, reject) {
+    //     var auditFile = '';
+    //     var xmlhttp = new XMLHttpRequest();
+    //     xmlhttp.onreadystatechange = function () {
+    //         if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
+    //             auditFile = xmlhttp.responseText;
+    //             resolve(auditFile)
+    //         }
+    //     };
+    //     // xmlhttp.open("GET", "https://gist.githubusercontent.com/mrdavey/993fc9e52c8a0f57b5e2b5dbaaf49c65/raw/e078ab9280ce487f72b5765230ce6e2689fcd999/audit_mock", true);
+    //     xmlhttp.open("GET", "https://gateway.ipfs.io/ipfs/QmV7FFUNZBqbxV6ba47D3Fj9APKZH1RCJYeRA3mw6XsGVM", true);
+
+    //     xmlhttp.send();
+    // })
 
 }
 
